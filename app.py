971216acf422fd17e3a3eb5d3e2b9691404828c4
ritvikmath <ritvikmath@g.ucla.edu>
2020-04-18 10:02:18 -0700

    first pass at dashboard

--- app.py
+++ app.py
@@ -1,63 +1,41 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkgreen'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+tabtitle='NLP Recipe Project'
+myheading='Enter a Recipe URL'
+recipe_link = 'https://www.allrecipes.com/recipe/20144/banana-banana-bread/'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
+########### App components
+
+recipe_input = dcc.Input(
+    id="recipe_input",
+    type="text",
+    placeholder="Enter Recipe URL"
 )
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
+recipe_output = html.Div(id="recipe_ouput")
 
 ########### Initiate the app
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
 ########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
+app.layout = html.Div(
+        [recipe_input, recipe_output]
 )
+
+@app.callback(
+    Output("recipe_output", "children"),
+    Input("recipe_input", "value"),
+)
+def cb_render(val):
+    return val
 
 if __name__ == '__main__':
     app.run_server()
